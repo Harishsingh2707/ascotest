368c3c8edfee5282ce95a340c751a43e2fcb981a
Harishsingh2707 <68434771+Harishsingh2707@users.noreply.github.com>
2021-09-23 16:40:20 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-sudo yum install -y gcc-c++ make 
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash - 
-sudo yum install -y nodejs 
 sudo npm install
 sudo npm run start:dev
 
