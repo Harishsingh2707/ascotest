8426efa558137615b84358069d2158095c2575ed
Harishsingh2707 <68434771+Harishsingh2707@users.noreply.github.com>
2021-09-23 19:30:33 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 pwd 
-cd /opt/codedeploy-agent/deployment-root/cbe443e6-087e-4492-8359-9703f2529a26/$DEPLOYMENT_ID/deployment-archive/
-ls -l
-sudo npm install
-sudo npm run start:dev
+cd /opt/codedeploy-agent/deployment-root/cbe443e6-087e-4492-8359-9703f2529a26/$DEPLOYMENT_ID/deployment-archive/ && npm install && npm run start:dev
+ls -l 
+
 
